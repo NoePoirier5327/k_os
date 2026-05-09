67b822a41c7aafba18ba9ed8403b1a20f3210b94
Noe Poirier <noer5327@gmail.com>
2026-05-09 19:13:10 +0200

    Implémentation de l'affichage des messages de panic.

--- src/lib.rs
+++ src/lib.rs
@@ -9,13 +9,14 @@ use core::arch::asm;
 // "no_mangle" garde le nom "_start" intact pour que l'assembleur le trouve
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    println!("Welcome to K Os.");
+    println!("Welcome to k_os.");
 
     hlt();
 }
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     hlt();
 }
 
