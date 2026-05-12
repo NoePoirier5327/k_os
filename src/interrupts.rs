5c073a236bfadc6141f1271ad23645e5b4b220a9
Noe Poirier <noer5327@gmail.com>
2026-05-12 12:33:03 +0200

    Ajout des interruptions du clavier.

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -19,6 +19,7 @@ pub static PICS: spin::Mutex<ChainedPics> = spin::Mutex::new(unsafe { ChainedPic
 #[repr(u8)]
 pub enum InterruptIndex {
     Timer = PIC_1_OFFSET,
+    Keyboard
 }
 
 impl InterruptIndex {
@@ -55,6 +56,12 @@ lazy_static! {
         // On référence la fonction de gestion du timer.
         idt[InterruptIndex::Timer.to_usize()].set_handler_fn(timer_interrupt_handler);
 
+        // On référence la fonction de gestion des entrées claviers.
+        // ATTENTION, pour l'instant on ne supporte que les ports ps2.
+        // Cependant, les ports USB sont émulés en ps2 donc pas de problème pour le moment.
+
+        idt[InterruptIndex::Keyboard.to_usize()].set_handler_fn(keyboard_interrupt_handler);
+
         idt
     };
 }
@@ -87,9 +94,45 @@ extern "x86-interrupt" fn double_fault_handler(stack_frame: InterruptStackFrame,
 /// # Argument
 /// * `stack_frame` : message d'interruption du timer.
 extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: InterruptStackFrame) {
-    print!(".");
+    //print!(".");
     unsafe {
       PICS.lock().notify_end_of_interrupt(InterruptIndex::Timer.to_u8());
+    }
+}
+
+/// Fonction de gestion des interruptions clavier.<br>
+/// Elle redonne la main au système après l'interruption.
+///
+/// # Argument
+/// * `stack_frame` : message d'interruption du clavier.
+extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: InterruptStackFrame) {
+    use pc_keyboard::{layouts, DecodedKey, HandleControl, Keyboard, ScancodeSet1};
+    use spin::Mutex;
+    use x86_64::instructions::port::Port;
+
+    lazy_static! {
+        static ref KEYBOARD: Mutex<Keyboard<layouts::Azerty, ScancodeSet1>> =
+            Mutex::new(Keyboard::new(ScancodeSet1::new(),
+                layouts::Azerty, HandleControl::Ignore)
+            );
+    }
+
+    let mut keyboard = KEYBOARD.lock();
+    let mut port = Port::new(0x60);
+
+    let scancode: u8 = unsafe { port.read() };
+    if let Ok(Some(key_event)) = keyboard.add_byte(scancode) {
+        if let Some(key) = keyboard.process_keyevent(key_event) {
+            match key {
+                DecodedKey::Unicode(character) => print!("{}", character),
+                DecodedKey::RawKey(key) => print!("{:?}", key),
+            }
+        }
+    }
+
+    unsafe {
+        PICS.lock()
+            .notify_end_of_interrupt(InterruptIndex::Keyboard.to_u8());
     }
 }
 
