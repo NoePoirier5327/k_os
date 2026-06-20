e2b061c6db6d5f631af8c258d1c1bb54c0481a7f
Noe Poirier <noer5327@gmail.com>
2026-06-20 23:22:57 +0200

    Agrandissement du tas.

--- kernel/allocator.rs
+++ kernel/allocator.rs
@@ -17,7 +17,7 @@ static ALLOCATOR: Locked<FixedSizeBlockAllocator> = Locked::new(FixedSizeBlockAl
 
 // Information de délimitation de la zone virtuelle du tas.
 pub const HEAP_START: usize = 0x_4444_4444_0000;
-pub const HEAP_SIZE: usize = 100 * 1024; // 100 KiB
+pub const HEAP_SIZE: usize = 100 * 512 * 1024; // 5 MiB
 
 
 /// Fonction cartographiant la zone mémoire du tas pour pouvoir y accéder plus tard. <br>
